cee81b5655e440b668b385a251f9d7230768f632
Marcus Roos <Marcusrs_roos@outlook.com>
2020-09-08 02:52:46 +0200

    Menu setup, menu disabling working as intended

---
+++ Laboration_1/include/MenuItem.h
@@ -0,0 +1,73 @@
+//
+// Created by Marcus Roos on 2020-09-08.
+// Mittuniversitet
+// StudentID: Maro1904
+//
+
+#ifndef DT060G_MENUITEM_H
+#define DT060G_MENUITEM_H
+#include <string>
+//------------------------------------------------------------------------------
+// Class definition
+//------------------------------------------------------------------------------
+/**
+ Class MenuItem
+
+ Keeps track of whether a menu is enabled or not, as well as the text of menu.
+ Contain members menutext of type string, and enabler of type bool.
+ Member functions include constructor, destructor, get and set functions.
+ */
+class MenuItem {
+private:
+std::string menutext;
+bool enabler;
+public:
+    /**
+* Default constructor of class MenuItem
+*
+* @param None.
+* @return None.
+*/
+    MenuItem();
+
+    /**
+* Constructor for initialization of class MenuItem
+*
+* @param string pmenutext and bool penabler
+* @return None.
+*/
+    MenuItem(std::string pmenutext, bool penabler);
+
+    /**
+* Destructor of class MenuItem
+*
+* @param None.
+* @return None.
+*/
+    ~MenuItem()=default;
+
+    /**
+* Sets value for class member enabler
+*
+* @param bool penabler
+* @return None.
+*/
+    void setEnabled(bool penabler);
+
+    /**
+* Returns value for class member menutext, constant
+* Inline
+* @param None.
+* @return menutext.
+*/
+    std::string getMenuText() const { return menutext;}
+
+    /**
+* Returns value for class member enabler, constant
+* Inline
+* @param None.
+* @return enabler.
+*/
+    bool getEnabled() const { return enabler; }
+};
+#endif
--- Laboration_1/src/main.cpp
+++ Laboration_1/src/main.cpp
@@ -1,7 +1,13 @@
-#include "Prototypes.h"
-#include "memstat.hpp"
+//
+// Created by Marcus Roos on 2020-09-08.
+// Mittuniversitet
+// StudentID: Maro1904
+//
+
+#include "MainMenu.h"
 
 int main() {
-    assignmentFunction();
+    MainMenu mainmenu;
+    mainmenu.run();
     return 0;
 }
