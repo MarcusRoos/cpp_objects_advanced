27b3933aad6ad4b51ca8f67ab4d5c13c6ee7ce1b
Marcus Roos <Marcusrs_roos@outlook.com>
2020-10-19 05:06:55 +0200

    Changed strategy, loading all into a vector, will use stringstream to split it up into parameters-

--- Project/src/station.cpp
+++ Project/src/station.cpp
@@ -18,29 +18,17 @@ Station::Station(std::string aStationname, std::vector<std::unique_ptr<Vehicle>>
 void Station::testStat() {
     const std::string startDelim="(", endDelim = ")";
 
-
-    int a=0, b=0, c=0, d=0;
+    std::vector<std::string> tmpStations;
     std::string Name;
     std::ifstream inFile("TrainStations.txt");
     if (inFile.is_open()){
-        getline(inFile, Name);
-        std::cout << "Name: " << Name << std::endl;
-            inFile >> a;
-            std::cout<< "A: " << a << std::endl;
-                stationName = "Grand Central";
-                inFile >> b;
-                inFile >> c;
-                inFile >> d;
-                inFile.get();
-                std::cout<< "B: " << b << std::endl;
-                std::cout<< "C: " << c << std::endl;
-                std::cout<< "D: " << d << std::endl;
-                stationVehicles.push_back(std::unique_ptr<Vehicle>(
-                        new CoachCar(a,b,c,d)));
-
-
-    }
+        while (!inFile.eof()){
+                getline(inFile, Name);
+                tmpStations.push_back(Name);
+            }
+        }
     std::cout << "TestStat" << std::endl;
-    std::cout << stationName << std::endl;
-    std::cout << stationVehicles[0]->getType() << std::endl;
+    for (int i=0; i<tmpStations.size(); i++){
+        std::cout << "Vector " << i << " : "<<tmpStations[i] << std::endl;
+    }
 }
