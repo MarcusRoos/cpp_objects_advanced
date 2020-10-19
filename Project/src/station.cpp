6973588d32083c98d520c0bb4e97b131a632ee5c
Marcus Roos <Marcusrs_roos@outlook.com>
2020-10-19 05:23:50 +0200

    Changed once more.

--- Project/src/station.cpp
+++ Project/src/station.cpp
@@ -16,19 +16,44 @@ Station::Station(std::string aStationname, std::vector<std::unique_ptr<Vehicle>>
 }
 
 void Station::testStat() {
-    const std::string startDelim="(", endDelim = ")";
+    const char startDelim='(', endDelim = ')';
+    std::string namn;
+    int aID, aType, c, d;
+    std::ifstream inFile("TrainStation.txt");
+    if (inFile.is_open()) {
+        getline(inFile, namn, startDelim);
 
-    std::vector<std::string> tmpStations;
-    std::string Name;
+        while (getline(inFile, tmpANr)) {
+            getline(inFile, tmpType);
+
+        }
+    }
+
+}
+
+
+
+/*
+    int a=0, b=0, c=0, d=0;
+    std::string Name, GrandC, Liege, Pancras, Dunedin, Milano, Luz, Shin;
     std::ifstream inFile("TrainStations.txt");
     if (inFile.is_open()){
-        while (!inFile.eof()){
-                getline(inFile, Name);
-                tmpStations.push_back(Name);
+        while (getline(inFile, Name)){
+            std::cout << "Name: " << Name << std::endl;
+            inFile >> a;
+            std::cout<< "A: " << a << std::endl;
+            if (a == 0) {
+                stationName = "Grand Central";
+                inFile.get();
+                stationVehicles.push_back(std::unique_ptr<Vehicle>(
+                        new CoachCar (1,1,1,1)));
+                GrandC = Name;
             }
         }
-    std::cout << "TestStat" << std::endl;
-    for (int i=0; i<tmpStations.size(); i++){
-        std::cout << "Vector " << i << " : "<<tmpStations[i] << std::endl;
     }
-}
+    std::cout << "TestStat" << std::endl;
+    std::cout << GrandC;
+    std::cout << stationName << std::endl;
+    std::cout << stationVehicles[0]->getType() << std::endl;
+    */
+
