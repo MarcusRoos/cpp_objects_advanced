fe5e2fe6de3ead480eb292c2e173f974f4a86059
Marcus Roos <Marcusrs_roos@outlook.com>
2020-10-26 05:42:20 +0100

    Disassemble, still causing a crash due to null value.

--- Project/src/train.cpp
+++ Project/src/train.cpp
@@ -48,7 +48,7 @@ bool Train::assembleVehicle(std::vector<std::shared_ptr<Station>> aStation) {
 
 void Train::disassembleTrain(std::vector<std::shared_ptr<Station>> aStation) {
     for (int k = 0; k < aStation.size(); k++) {
-        if (aStation[k]->getStationname() == toStation) {
+        if (aStation[k]->getStationname() == toStation && trainVehicles[k] != NULL) {
             aStation[k]->incomingVehicle(trainVehicles);
         }
     }
